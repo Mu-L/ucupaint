4c644eb29b6ef236febaad9f21adcb94f7099cf8
Yusuf Umar <yusufumaris@gmail.com>
2026-05-04 20:22:10 +0700

    Use `bmesh` instead of `bpy.ops` for flipping normal. This will improve performance in most cases.

--- bake_common.py
+++ bake_common.py
@@ -1667,6 +1667,27 @@ def create_plane_object():
 
     return obj
 
+def flip_mesh_normals(obj):
+    if not obj or obj.type != 'MESH': return
+
+    # Create BMesh from object data
+    bm = bmesh.new()
+    bm.from_mesh(obj.data)
+
+    if is_bl_newer_than(2, 80):
+        for face in bm.faces:
+            face.normal_flip()
+    else:
+        for face in bm.faces:
+            bmesh.utils.face_flip(face)
+
+    # Write changes back to the mesh
+    bm.to_mesh(obj.data)
+    bm.free()
+    
+    # Update viewport to show changes
+    obj.data.update()
+
 def fxaa_image(image, alpha_aware=True, bake_device='CPU', first_tile_only=False):
     T = time.time()
     print('FXAA: Doing FXAA pass on', image.name + '...')
@@ -3219,32 +3240,13 @@ def bake_to_entity(bprops, overwrite_img=None, segment=None):
                 flow_vcol = get_flow_vcol(ob, main_uv, straight_uv)
 
     # Check if doing actual flip normals operator is needed
-    need_flip_normals_ops = bprops.flip_normals and (bprops.type != 'AO' or alpha_outp) and is_bl_newer_than(2, 80)
+    need_flip_normals = bprops.flip_normals and (bprops.type != 'AO' or alpha_outp or not is_bl_newer_than(2, 80))
 
     # Flip normals setup
-    if need_flip_normals_ops:
-        #ori_mode[obj.name] = obj.mode
-        if is_bl_newer_than(2, 80):
-            # Deselect other objects first
-            for o in other_objs:
-                o.select_set(False)
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.reveal()
-            bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.flip_normals()
-            bpy.ops.object.mode_set(mode='OBJECT')
-            # Reselect other objects
-            for o in other_objs:
-                o.select_set(True)
-        else:
-            for ob in objs:
-                if ob in other_objs: continue
-                scene.objects.active = ob
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.reveal()
-                bpy.ops.mesh.select_all(action='SELECT')
-                bpy.ops.mesh.flip_normals()
-                bpy.ops.object.mode_set(mode='OBJECT')
+    if need_flip_normals:
+        for ob in objs:
+            if ob in other_objs: continue
+            flip_mesh_normals(ob)
 
     # More setup
     ori_mods = {}
@@ -4190,32 +4192,10 @@ def bake_to_entity(bprops, overwrite_img=None, segment=None):
             if vcol: vcols.remove(vcol)
 
     # Recover flip normals setup
-    if need_flip_normals_ops:
-        #bpy.ops.object.mode_set(mode = 'EDIT')
-        #bpy.ops.mesh.flip_normals()
-        #bpy.ops.mesh.select_all(action='DESELECT')
-        #bpy.ops.object.mode_set(mode = ori_mode)
-        if is_bl_newer_than(2, 80):
-            # Deselect other objects first
-            for o in other_objs:
-                o.select_set(False)
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.reveal()
-            bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.flip_normals()
-            bpy.ops.object.mode_set(mode='OBJECT')
-            # Reselect other objects
-            for o in other_objs:
-                o.select_set(True)
-        else:
-            for ob in objs:
-                if ob in other_objs: continue
-                scene.objects.active = ob
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.reveal()
-                bpy.ops.mesh.select_all(action='SELECT')
-                bpy.ops.mesh.flip_normals()
-                bpy.ops.object.mode_set(mode='OBJECT')
+    if need_flip_normals:
+        for ob in objs:
+            if ob in other_objs: continue
+            flip_mesh_normals(ob)
 
     # Recover subdiv setup
     if height_root_ch and subdiv_setup_changes:
